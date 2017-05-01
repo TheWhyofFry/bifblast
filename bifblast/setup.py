b250dac3e6ff53f0e5a38265c3bd18b286966d57
Monk <werner.smidt@gmail.com>
2017-05-01 20:08:13 +0200

    Initial commit of the bare package

---
+++ bifblast/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="BifBLAST!", version="0.0",
+      description="BCBU Web-based BLAST project",
+      url="https://github.com/TheWhyofFry/bifblast",
+      author="See MAINTAINERS.md",
+      license="GPL",
+      packages=["bifblast"],
+      install_requires=["biopython"])
+
+
